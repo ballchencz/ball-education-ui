fe9759e49912f869a61ef52a8f524f4001382371
chenzhao <allwhiteonlyblack@163.com>
2016-09-01 18:25:28 +0800

    study

--- js/index-url.js
+++ js/index-url.js
@@ -5,7 +5,9 @@ define(function(){
     var URL = {
         "getRealWarCourse":"../data/realWarCourse.json",//获得实战课程数据
         "getFreeWellCourse":"../data/freeWellCourse.json",//获得免费好课课程
-        "getStudyResources":"../data/studyResources.json"//获得学习资源数据
+        "getStudyResources":"../data/studyResources.json",//获得学习资源数据
+        "getCourseCategory":"../data/courseCategory.json",//获得课程分类数据
+        "getCyclePicture":"../data/cyclePicture.json"//获得轮播图数据
     };
     return URL;
 });
--- js/index.js
+++ js/index.js
@@ -2,15 +2,53 @@
  * Created by ballchen on 2016/8/30.
  */
 require(['./js/index-url','angular','jquery','jquery-super-slide','angular-ui-router'],function(URL,angular,$){
-    /*-------页面效果js-----------------*/
-    $('.tab_qiehuan').hover(function(){
-        $(this).children(".bianhuabeijing").css("background-color","#8a8f93").next().css("display","block")
-    },function(){
-        $(this).children(".bianhuabeijing").css("background-color","#a9aaae").next().css("display","none")
-    });
-    $(".focusBox").slide({ mainCell:".pic",effect:"left", autoPlay:true, delayTime:500});
 
     var index = angular.module('index',[]);
+
+    /*循环完成filter*/
+    index.directive('onFinishRenderFilters', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function() {
+                        scope.$emit('ngRepeatFinished');
+                    });
+                }
+            }
+        };
+    });
+    /*课程分类*/
+    index.controller('courseCategory',['$scope','$http',function($scope,$http){
+        $http.get(URL.getCourseCategory,[]).success(function(data,status,headers,config){
+            $scope.courseCategorys = data;
+        }).error(function(data,status,headers,config){
+        });
+        $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+            //渲染完成，加载事件
+            $('.tab_qiehuan').hover(function(){
+                $(this).children(".bianhuabeijing").css("background-color","#8a8f93").next().css("display","block")
+            },function(){
+                $(this).children(".bianhuabeijing").css("background-color","#a9aaae").next().css("display","none")
+            });
+        });
+    }]);
+
+    /**
+     * 轮播图
+     */
+    index.controller('cyclePicture',['$scope','$http',function($scope,$http){
+        $http.get(URL.getCyclePicture,[]).success(function(data,status,headers,config){
+            console.log(data);
+            $scope.cyclePictures = data;
+        }).error(function(data,status,headers,config){
+        });
+        $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+            //渲染完成，加载事件
+            $(".focusBox").slide({ mainCell:".pic",effect:"left", autoPlay:true, delayTime:500});
+        });
+    }]);
+
 
     /*学习资源*/
     index.controller('studyResources',['$scope','$http',function($scope,$http){
@@ -39,4 +77,23 @@ require(['./js/index-url','angular','jquery','jquery-super-slide','angular-ui-ro
     }]);
 
     angular.bootstrap(document,['index']);
+    /*-------页面效果js-----------------*/
+/*    $('.tab_qiehuan').hover(function(){
+        alert(1);
+        $(this).children(".bianhuabeijing").css("background-color","#8a8f93").next().css("display","block")
+    },function(){
+        alert(12);
+        $(this).children(".bianhuabeijing").css("background-color","#a9aaae").next().css("display","none")
+    });
+    $(".focusBox").slide({ mainCell:".pic",effect:"left", autoPlay:true, delayTime:500});*/
+/*    $('.tab_qiehuan').on("mouseover mouseout",function(event){
+        console.log(event);
+        if(event.type == "mouseover"){
+            //鼠标悬浮
+            console.log("mouseover")
+        }else if(event.type == "mouseout"){
+            //鼠标离开
+            console.log("mouseover")
+        }
+    });*/
 });
