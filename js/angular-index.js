72101dbf33dbf97c78b373a40a30d9bd9d7b24f7
chenzhao <allwhiteonlyblack@163.com>
2016-09-02 17:55:29 +0800

    完善分类数据

--- js/angular-index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ballchen on 2016/8/30.
- */
-
-var index = angular.module('index',[]);
-index.controller('otherController',['$scope',function($scope){
-    $scope.templates = [{name:"other",url:"./other-include.html"}];
-    $scope.template = $scope.templates[0];
-}]);
